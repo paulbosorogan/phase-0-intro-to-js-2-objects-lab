d4be4c13aa032b3451e6e7301e6cd1c7fa9629b0
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-28 20:39:43 -0400

    commit tree 473062ef5d3c8167c090ad687b82817165436567

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = { name: ''};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, secondKey, secondValue){
+    employee[secondKey] = secondValue;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+
